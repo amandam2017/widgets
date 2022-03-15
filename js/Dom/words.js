5008273a9e707bdb8058d88247213ea0a17a07a6
amandam2017 <amandamaarman19911@gmail.com>
2022-03-15 14:12:42 +0200

    first commit

---
+++ js/Dom/words.js
@@ -0,0 +1,14 @@
+// reference elements: 
+// input field to get the value or the entered sentence
+// add btn to do something onclick
+const sentenceElem = document.querySelector('.sentence');
+const addBtnElem = document.querySelector('.addBtn');
+
+const addSentence = ()=>{
+
+    const userWords = sentenceElem.value;
+    if(userWords){
+        
+    } 
+
+}
