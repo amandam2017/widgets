27c882d1823252aacd1b5d63e50910bb3dd96b46
amandam2017 <amandamaarman19911@gmail.com>
2022-03-15 15:51:42 +0200

    separated the logic and now trying to split the words in a sentence

--- js/Dom/words.js
+++ js/Dom/words.js
@@ -1,24 +1,36 @@
 // reference elements: 
 // input field to get the value or the entered sentence
-
 const sentenceElem = document.querySelector('.sentence');
 const addBtnElem = document.querySelector('.addBtn');
 const displaySentenceElem = document.querySelector('.displaySentence');
+
+const copysentence = 'Hello there! Do you know Amanda Maarman from Stellenbosch';
+
 
 //REFERENCE THE TEMPLATE
 
 //COMPILE THE TEMPLATE
 
-const addSentence = ()=>{
-    const userWords = sentenceElem.value;
-    if(userWords != ''){
-        console.log(userWords);
+//INSTANTIATE FACTORY
+let Factory = Words(); 
 
-        displaySentenceElem.innerHTML = userWords;
+const addSentence = ()=>{
+    var addedSentences = [];
+    
+    const userWords = sentenceElem.value;
+    if(userWords){
+        displaySentenceElem.innerHTML = Factory.addWords(userWords);
+        console.log(displaySentenceElem.innerHTML);
+
+        addedSentences.push(displaySentenceElem.innerHTML);
+        addedSentences = displaySentenceElem.innerHTML.split();
+
+        console.log(addedSentences = displaySentenceElem.innerHTML.split());
+
     
     } 
 
 }
 
 // add event listener on btn to do something onclick
-addBtnElem.addEventListener('click', addSentence)
+addBtnElem.addEventListener('click', addSentence);
---
+++ js/Factories/wordsff.js
@@ -0,0 +1,19 @@
+const Words = () =>{
+    const addWords = (userSentence)=>{
+        // const addedWords = [];
+
+        if(userSentence != ''){
+            console.log(userSentence);
+            // addedWords.push(userSentence);
+            // console.log(addedWords);
+
+            return userSentence;
+
+        
+        } 
+    }
+
+    return{
+        addWords,
+    }
+}
