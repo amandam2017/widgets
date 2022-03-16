2cb755b597a25621542e82da77bcfe186e1756bf
amandam2017 <amandamaarman19911@gmail.com>
2022-03-16 15:53:02 +0200

    hiding the words with characters less than 5

--- js/Dom/words.js
+++ js/Dom/words.js
@@ -6,6 +6,8 @@ const sentenceElem = document.querySelector('.sentence');
 const addBtnElem = document.querySelector('.addBtn');
 const displaySentenceElem = document.querySelector('.displaySentence');
 const wordLengthElem = document.querySelector('.wordLength');
+
+const checkboxElem = document.getElementById('checkbox');
 const shortWordsElem = document.querySelector('.shortWords');
 
 const copysentence = 'Hello there! Do you know Amanda Maarman from Stellenbosch';
@@ -13,9 +15,12 @@ const copysentence = 'Hello there! Do you know Amanda Maarman from Stellenbosch'
 //INSTANTIATE FACTORY
 let Factory = Words(); 
 
-const addSentence = (option)=>{
+const addSentence = ()=>{
     
+let highlighted = '';
+
     const userWords = sentenceElem.value;
+
     if(userWords){
         displaySentenceElem.innerHTML = Factory.addWords(userWords);
     }
@@ -23,28 +28,52 @@ const addSentence = (option)=>{
     // split is to split each word in a sentence and return an array of words
     let addedWords = userWords.split(" ");
 
-        let highlighted = '';
-        let hiddenW = '';
-
-
         for (let i = 0; i < addedWords.length; i++) {
             const wordsToHighlight = addedWords[i];
+            if(userWords){
+                shortWordsElem.innerHTML = Factory.addWords(userWords);
+            }
 
             if(wordsToHighlight.length>4){
                 highlighted += `<mark style= 'background-color:yellow'>${wordsToHighlight}</mark>`
-
             }
             else{
-                highlighted += wordsToHighlight;
+                highlighted += wordsToHighlight + " ";
             }
 
-            displaySentenceElem.innerHTML = highlighted;
+            shortWordsElem.innerHTML = highlighted;
             wordLengthElem.innerHTML = `Your sentence have: ${addedWords.length} words.`;
             
     }
 
 }
 
+const hideAndHighlight =()=>{
+    
+    let changeHighlighted = '';
+    const userWords = sentenceElem.value;
+
+    // split is to split each word in a sentence and return an array of words
+    let addedWords = userWords.split(" ");
+
+        for (let i = 0; i < addedWords.length; i++) {
+            const wordsToHighlight = addedWords[i];
+            if(userWords){
+                shortWordsElem.innerHTML = Factory.addWords(userWords);
+            }
+            if((checkboxElem.checked === true) && (wordsToHighlight.length>5)){
+                changeHighlighted += `<mark style= 'background-color:yellow'>${wordsToHighlight}</mark>`
+            }
+            else{
+                changeHighlighted += " ";
+            }
+
+            shortWordsElem.innerHTML = changeHighlighted;
+            
+    }
+    
+}
+
 // add event listener on btn to do something onclick
 addBtnElem.addEventListener('click', addSentence);
-// shortWordsElem.addEventListener('click', hide);
+checkboxElem.addEventListener('click', hideAndHighlight);
